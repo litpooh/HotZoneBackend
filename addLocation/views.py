0e2d3393e96f08b1679342949ae212e5b582161e
marcozzxx810 <marcowingwing@gmail.com>
2020-11-21 22:48:24 +0800

    updated index page

--- addLocation/views.py
+++ addLocation/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import VisitedLocation
+from .models import VisitedLocation, Location
 from .serializers import VisitedLocationSerializer
 from django.views.generic import TemplateView
 from .retrieveLocation import searchAndCompareLocation

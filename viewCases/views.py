c668e49484642930af3c0dd9a5b95d1d1705fe37
marcozzxx810 <marcowingwing@gmail.com>
2020-11-21 23:33:32 +0800

    added a input for case select

--- viewCases/views.py
+++ viewCases/views.py
@@ -86,4 +86,5 @@ class IndexView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['cases']= CaseRecord.objects.all()
         return context
